2d6c3060c11367466002255f6562bbad7da8714f
atbu <alexburrows658@gmail.com>
2025-01-15 16:00:26 +0000

    Expand user detail to display all of a user's posts

--- browse/views.py
+++ browse/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from .models import Post
@@ -5,12 +6,17 @@ from .models import Post
 # Create your views here.
 def index(request):
   posts = Post.objects.order_by("-created_at")
-  context = { 'posts': posts, }
-  return render(request, 'browse/index.html', context)
+  return render(request, 'browse/index.html', {'posts': posts,})
 
 def post_detail(request, post_id):
   post = get_object_or_404(Post, pk=post_id)
-  return render(request, "browse/post_detail.html", {"post": post})
+  return render(request, "browse/post_detail.html", {"post": post,})
 
 def user_detail(request, username):
-  return HttpResponse("You're looking at %s's posts." % username)
+  user = get_object_or_404(get_user_model(), username=username)
+  posts = Post.objects.filter(author__username=username)
+  context = {
+    "user": user,
+    "posts": posts,
+  }
+  return render(request, "browse/user_detail.html", context)
