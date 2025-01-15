e63675d166cf10d2ad276a85c202f88f432b0448
atbu <alexburrows658@gmail.com>
2025-01-15 15:02:21 +0000

    Add basic model definitions

--- browse/models.py
+++ browse/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
+from uuid import uuid4
+
 # Create your models here.
+class Post(models.Model):
+  uniqueId = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+  content = models.CharField(max_length=280)
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  createdAt = models.DateTimeField()
+
+class Like(models.Model):
+  uniqueId = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+  liker = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
