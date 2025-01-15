6bfb730ba4c22c3a27a4ccaa68ee966415604ea9
atbu <alexburrows658@gmail.com>
2025-01-15 15:14:07 +0000

    Attempt to update index view, rename createdAt to created_at

--- browse/models.py
+++ browse/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
   uniqueId = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   content = models.CharField(max_length=280)
   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-  createdAt = models.DateTimeField()
+  created_at = models.DateTimeField()
 
 class Like(models.Model):
   uniqueId = models.UUIDField(primary_key=True, default=uuid4, editable=False)
--- browse/views.py
+++ browse/views.py
@@ -1,9 +1,14 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Welcome to the browse index.")
+  posts = Post.objects.order_by("-created_at")
+  output = ""
+  for p in posts:
+    output += "<h3>%s <strong>(by %s)</strong></h3><br />" % p.author.username
+  return HttpResponse(output)
 
 def detail(request, post_id):
   return HttpResponse("You're looking at post %s." % post_id)
