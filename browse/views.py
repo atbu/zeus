9fa33cfd7c0b34e09c7b8ba461b12b325be88ac7
atbu <alexburrows658@gmail.com>
2025-01-15 15:10:11 +0000

    Add models to admin site, add detail view

--- browse/views.py
+++ browse/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
   return HttpResponse("Welcome to the browse index.")
+
+def detail(request, post_id):
+  return HttpResponse("You're looking at post %s." % post_id)
