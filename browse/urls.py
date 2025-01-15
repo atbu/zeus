9eead5917c22ace07a87e05ec31319659a7904c4
atbu <alexburrows658@gmail.com>
2025-01-15 15:43:59 +0000

    Map URLs to post_detail and user_detail, configure views and setup basic template

--- browse/urls.py
+++ browse/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
-  path('<uuid:post_id>/', views.detail, name='detail'),
+  path('p/<uuid:post_id>/', views.post_detail, name='post_detail'),
+  path('u/<str:username>/', views.user_detail, name='user_detail'),
 ]
--- browse/views.py
+++ browse/views.py
@@ -1,14 +1,15 @@
-# from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Post
 
 # Create your views here.
 def index(request):
   posts = Post.objects.order_by("-created_at")
-  output = ""
-  for p in posts:
-    output += "<h3>%s <strong>(by %s)</strong></h3><br />" % p.author.username
-  return HttpResponse(output)
+  context = { 'posts': posts, }
+  return render(request, 'browse/index.html', context)
 
-def detail(request, post_id):
+def post_detail(request, post_id):
   return HttpResponse("You're looking at post %s." % post_id)
+
+def user_detail(request, username):
+  return HttpResponse("You're looking at %s's posts." % username)
