4d0ff31b2928cf24de7354c7b6e5fe8a00e456f9
atbu <alexburrows658@gmail.com>
2025-01-15 15:49:10 +0000

    Update detail view to recognize 404s + change template accordingly

--- browse/views.py
+++ browse/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 # Create your views here.
@@ -9,7 +9,8 @@ def index(request):
   return render(request, 'browse/index.html', context)
 
 def post_detail(request, post_id):
-  return HttpResponse("You're looking at post %s." % post_id)
+  post = get_object_or_404(Post, pk=post_id)
+  return render(request, "browse/post_detail.html", {"post": post})
 
 def user_detail(request, username):
   return HttpResponse("You're looking at %s's posts." % username)
